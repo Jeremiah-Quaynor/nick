5aaeecea2c45987114ba7c7b6ec5450267fcf76f
CharlesBoadu <charles.boadu@stu.ucc.edu.gh>
2023-12-06 11:30:38 +0000

    Adding Responsiveness to Team Page

--- components/TeamMembers/TeamItem.jsx
+++ components/TeamMembers/TeamItem.jsx
@@ -17,60 +17,113 @@ function TeamItem({
   linkedInLink,
   setShowModal,
   setData,
-  details
+  details,
 }) {
   return (
-    <div
-      className="col-xl-4 col-md-6 col-12 hover:cursor-pointer"
-      onClick={() => {
-        setShowModal(true);
-        setData({
-          name: name,
-          img: img,
-          position: position,
-          details: details
-        });
-      }}
-    >
+    <>
       <div
-        className={`single-team-member`}
-        style={{ backgroundColor: "white" }}
+        className="col-xl-4 col-md-6 col-12 hover:cursor-pointer hidden sm:block"
+        onClick={() => {
+          setShowModal(true);
+          setData({
+            name: name,
+            img: img,
+            position: position,
+            details: details,
+          });
+        }}
       >
-        <div className="member-img">
-          <Image
-            src={img}
-            alt="member"
-            className={
-              name.toUpperCase() === "ALIDU ZAKARIA" || name.toUpperCase() === "MARTIN OHENE ANIM"
-                ? `w-screen h-screen object-cover mt-[-32%]`
-                : name.toUpperCase()  === "AGNES KWAKYE"
-                ? `w-screen h-screen object-cover mt-[-24%]`
-                : `w-screen h-screen object-cover mt-[-12%]`
-            }
-          />
-          {/* <img src={img} alt="member" /> */}
-        </div>
-        <div className="member-details">
-          <span>{position}</span>
-          <h3>{name}</h3>
-        </div>
-        <div className="social-icons">
-          <div className="plus-icon pl-[15%] pt-[15%]">
-            <FaPlus />
+        <div
+          className={`single-team-member`}
+          style={{ backgroundColor: "white" }}
+        >
+          <div className="member-img">
+            <Image
+              src={img}
+              alt="member"
+              className={
+                name.toUpperCase() === "ALIDU ZAKARIA" ||
+                name.toUpperCase() === "MARTIN OHENE ANIM"
+                  ? `w-screen h-screen object-cover mt-[-32%]`
+                  : name.toUpperCase() === "AGNES KWAKYE"
+                  ? `w-screen h-screen object-cover mt-[-24%]`
+                  : `w-screen h-screen object-cover mt-[-12%]`
+              }
+            />
+            {/* <img src={img} alt="member" /> */}
           </div>
-          <div className="top">
-            <a className="pl-[35%] pt-[35%]">
-              <FaFacebookF />
-            </a>
+          <div className="member-details">
+            <span>{position}</span>
+            <h3>{name}</h3>
           </div>
-          <div className="right">
-            <a className="pl-[35%] pt-[35%]">
-              <FaLinkedinIn />
-            </a>
+          <div className="social-icons">
+            <div className="plus-icon pl-[15%] pt-[15%]">
+              <FaPlus />
+            </div>
+            <div className="top">
+              <a className="pl-[35%] pt-[35%]">
+                <FaFacebookF />
+              </a>
+            </div>
+            <div className="right">
+              <a className="pl-[35%] pt-[35%]">
+                <FaLinkedinIn />
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <div
+        className="col-xl-4 col-md-6 col-12 hover:cursor-pointer block md:hidden"
+        onClick={() => {
+          setShowModal(true);
+          setData({
+            name: name,
+            img: img,
+            position: position,
+            details: details,
+          });
+        }}
+      >
+        <div
+          className={`single-team-member`}
+          style={{ backgroundColor: "white" }}
+        >
+          <div className="member-img">
+            <Image
+              src={img}
+              alt="member"
+              className={
+                name.toUpperCase() === "ALIDU ZAKARIA"
+                  ? `w-screen h-screen object-cover mt-[-60%]`
+                  : `w-full h-full object-cover`
+              }
+            />
+            {/* <img src={img} alt="member" /> */}
+          </div>
+          <div className="member-details">
+            <span>{position}</span>
+            <h3>{name}</h3>
+          </div>
+          <div className="social-icons">
+            <div className="plus-icon pl-[15%] pt-[15%]">
+              <FaPlus />
+            </div>
+            <div className="top">
+              <a className="pl-[35%] pt-[35%]">
+                <FaFacebookF />
+              </a>
+            </div>
+            <div className="right">
+              <a className="pl-[35%] pt-[35%]">
+                <FaLinkedinIn />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- components/TeamMembers/index.jsx
+++ components/TeamMembers/index.jsx
@@ -22,7 +22,8 @@ function TeamMembers() {
   return (
     <>
       {showModal && (
-        <div>
+        <>
+        <div className="hidden sm:block">
           <div className="fixed z-50 inset-0 overflow-y-auto font-montserrat">
             <div className={data.name === "Nick Danso Adjei" ? `h-screen sm:block sm:p-0 bg-black/40` : `h-fit 2xl:h-screen sm:block sm:p-0 bg-black/40`}>
               <div className="flex flex-col p-10 animate-in slide-in-from-top duration-700">
@@ -68,6 +69,55 @@ function TeamMembers() {
             </div>
           </div>
         </div>
+
+        <div className="block md:hidden">
+          <div className="fixed z-50 inset-0 overflow-y-auto font-montserrat">
+            <div className={data.name === "Nick Danso Adjei" ? `h-screen sm:block sm:p-0 bg-black/40` : `h-fit 2xl:h-screen sm:block sm:p-0 bg-black/40`}>
+              <div className="flex flex-col p-2 animate-in slide-in-from-top duration-700">
+                <div
+                  className="flex justify-end mb-4 hover:cursor-pointer text-white hover:text-[#f608e2]"
+                  onClick={handleCloseModal}
+                >
+                  <IoMdClose size={30} />
+                </div>
+                <div className="bg-white rounded-lg p-8 mx-auto shadow-sm ">
+                  <div className="w-48 h-48 rounded-full bg-gray-200/20 mx-auto">
+                    <Image
+                      src={data.img.src}
+                      alt="User Image"
+                      className="h-full w-full rounded-full object-cover"
+                      width={500}
+                      height={500}
+                    />
+                  </div>
+                  <div>
+                    <div className="mt-5 flex items-center justify-center flex-col ">
+                      <div className="space-y-4">
+                        <div className="flex items-center flex-col">
+                          <div className="px-10 py-1 rounded-lg bg-[#0a113b] w-fit text-xl text-white uppercase mb-2">
+                            Name
+                          </div>
+                          <div className="my-auto font-semibold text-[#ff5e14] text-lg">{data.name}</div>
+                        </div>
+                        <div className="flex items-center flex-col">
+                          <div className="px-10 py-1 rounded-lg bg-[#0a113b] text-xl text-white uppercase mb-2">
+                            Position
+                          </div>
+                          <div className="my-auto font-semibold text-[#ff5e14] text-lg">{data.position}</div>
+                        </div>
+                        <div>
+                          {data.details}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        </>
       )}
       <section className="">
         <h1 className="text-center text-6xl font-bold tracking-normal text-[#f4e733] bg-black/50 py-20 bg-gradient-to-r from-[#0a113b] to-[#d7454d]">
